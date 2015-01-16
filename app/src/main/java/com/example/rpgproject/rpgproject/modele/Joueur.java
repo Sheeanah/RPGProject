8f93127e20225b1a42f2fb61eae4a9f3a2ae37b0
kalazard <alazardandroid@gmail.com>
2015-01-16 15:06:54 +0100

    Test

--- app/src/main/java/com/example/rpgproject/rpgproject/modele/Joueur.java
+++ app/src/main/java/com/example/rpgproject/rpgproject/modele/Joueur.java
@@ -4,8 +4,15 @@ package com.example.rpgproject.rpgproject.modele;
  * Created by Dorian on 16/01/2015.
  */
 public class Joueur extends Personnage {
+
+
     private int or;
     private int xp;
+
+    public Joueur(int xp, int or) {
+        this.xp = xp;
+        this.or = or;
+    }
 
     public int getXp() {
         return xp;
@@ -21,5 +28,30 @@ public class Joueur extends Personnage {
 
     public void setOr(int or) {
         this.or = or;
+    }
+
+    @Override
+    public int getLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getVie() {
+        return 0;
+    }
+
+    @Override
+    public int getAttaque() {
+        return 0;
+    }
+
+    @Override
+    public int getDefense() {
+        return 0;
+    }
+
+    @Override
+    public float getChance() {
+        return 0;
     }
 }
--- app/src/main/java/com/example/rpgproject/rpgproject/modele/Personnage.java
+++ app/src/main/java/com/example/rpgproject/rpgproject/modele/Personnage.java
@@ -3,5 +3,16 @@ package com.example.rpgproject.rpgproject.modele;
 /**
  * Created by Dorian on 16/01/2015.
  */
-public class Personnage {
+public abstract class Personnage {
+
+    public abstract int getLevel();
+
+    public abstract int getVie();
+
+    public abstract int getAttaque();
+
+    public abstract int getDefense();
+
+    public abstract float getChance();
+
 }
