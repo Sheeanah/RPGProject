4272e356c7707659331a5f322fe74101a521e8b7
Sheeanah <dorian.sutter@free.fr>
2015-01-16 13:58:13 +0100

    Fichiers de base controleur/modele

---
+++ app/src/androidTest/java/com/example/rpgproject/rpgproject/controleur/BaseControleur.java
@@ -0,0 +1,7 @@
+package com.example.rpgproject.rpgproject.controleur;
+
+/**
+ * Created by Dorian on 16/01/2015.
+ */
+public class BaseControleur {
+}
---
+++ app/src/androidTest/java/com/example/rpgproject/rpgproject/modele/BaseModele.java
@@ -0,0 +1,7 @@
+package com.example.rpgproject.rpgproject.modele;
+
+/**
+ * Created by Dorian on 16/01/2015.
+ */
+public class BaseModele {
+}
