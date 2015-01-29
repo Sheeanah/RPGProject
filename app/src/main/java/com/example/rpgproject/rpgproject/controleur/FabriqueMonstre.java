2912b0e2cac9a887c175663f983d316edc3730f2
Sheeanah <dorian.sutter@free.fr>
2015-01-29 20:48:00 +0100

    ajout de getmonstre avec string et de la liste de monstres

--- app/src/main/java/com/example/rpgproject/rpgproject/controleur/FabriqueMonstre.java
+++ app/src/main/java/com/example/rpgproject/rpgproject/controleur/FabriqueMonstre.java
@@ -17,50 +17,71 @@ public class FabriqueMonstre{
     private static FabriqueMonstre uniqueInstance;
     private static List<String> lstMonstres;
 
-        FabriqueMonstre()
+    FabriqueMonstre()
+    {
+        super();
+        lstMonstres =new ArrayList<String>();
+        lstMonstres.add("com.example.rpgproject.rpgproject.modele.Monstres.Gobelin");
+        lstMonstres.add("com.example.rpgproject.rpgproject.modele.Monstres.Orc");
+    }
+
+    public Monstre getMonstre(int monstreId,Context context) {
+        Monstre res=null;
+        if(lstMonstres.size()>monstreId)
         {
-            super();
-            lstMonstres =new ArrayList<String>();
-            lstMonstres.add("com.example.rpgproject.rpgproject.modele.Monstres.Gobelin");
-            lstMonstres.add("com.example.rpgproject.rpgproject.modele.Monstres.Orc");
-        }
-
-        public Monstre getMonstre(int monstreId,Context context) {
-            Monstre res=null;
-            if(lstMonstres.size()>monstreId)
-            {
-                try{
-                    Class newClass=Class.forName(lstMonstres.get(monstreId));
-                    res=(Monstre)newClass.getConstructor(Context.class).newInstance(context);
-                }
-                catch (Exception e){
-                    e.printStackTrace();
-                    Log.e("FabriqueObjet", e.getMessage());
-                }
+            try{
+                Class newClass=Class.forName(lstMonstres.get(monstreId));
+                res=(Monstre)newClass.getConstructor(Context.class).newInstance(context);
             }
-            return res;
-        }
-
-        public Monstre getMonstre(String nomMonstre,Context context){
-            Monstre res=null;
-            for(String str : lstMonstres){
-                String[] strSplit=str.split(".");
-                if(strSplit[strSplit.length-1].equals(nomMonstre)){
-                    res=getMonstre(lstMonstres.indexOf(str),context);
-                }
+            catch (Exception e){
+                e.printStackTrace();
+                Log.e("FabriqueObjet", e.getMessage());
             }
-            return res;
         }
+        return res;
+    }
 
-        public int getIdObjet(String monstreClass){
-            return lstMonstres.indexOf(monstreClass);
-        }
-
-        public static FabriqueMonstre getUniqueInstance()
-        {
-            if(uniqueInstance==null){
-                uniqueInstance=new FabriqueMonstre();
+    public Monstre getMonstre(String nomMonstre,Context context){
+        Monstre res=null;
+        for(String str : lstMonstres){
+            String[] strSplit=str.split(".");
+            if(strSplit[strSplit.length-1].equals(nomMonstre)){
+                res=getMonstre(lstMonstres.indexOf(str),context);
             }
-            return uniqueInstance;
         }
+        return res;
+    }
+
+    public int getIdObjet(String monstreClass){
+        return lstMonstres.indexOf(monstreClass);
+    }
+
+    public static FabriqueMonstre getUniqueInstance()
+    {
+        if(uniqueInstance==null){
+            uniqueInstance=new FabriqueMonstre();
+        }
+        return uniqueInstance;
+    }
+
+    public Monstre getObjet(String nomMonstre,Context context){
+        Monstre res=null;
+        for(String str : lstMonstres){
+            String[] strSplit=str.split("\\.");
+            if(strSplit[strSplit.length-1].equals(nomMonstre)){
+                res=getMonstre(lstMonstres.indexOf(str), context);
+            }
+        }
+        return res;
+    }
+
+    public List<Monstre> getMonstres(Context context){
+        int i=0;
+        List<Monstre> res=new ArrayList<Monstre>();
+        while(i<lstMonstres.size()){
+            res.add(getMonstre(i,context));
+            i++;
+        }
+        return res;
+    }
 }
